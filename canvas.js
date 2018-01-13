6f9a13dfe50e4860e1639dce93a75135c760e3f8
Alex Yang <alexyang@Alexs-Air.hitronhub.home>
2018-01-12 18:31:58 -0800

    javascript

--- canvas.js
+++ canvas.js
@@ -1,8 +1,7 @@
 var canvas;
-canvas = document.getElementById("my-canvas"); 
 var ctx;
-ctx = canvas.getContext('2d');
 window.onload = init;
 function init() {
-
-} 
+    canvas = document.getElementById("my-canvas")
+    ctx = canvas.getContext('2d')
+}
