10f635f23b30a5edd18ad57d108db0e372d4921b
Alex Yang <alexyang@Alexs-Air.hitronhub.home>
2018-01-19 17:44:34 -0800

    JS

--- canvas.js
+++ canvas.js
@@ -2,8 +2,10 @@ var canvas;
 var ctx;
 window.onload = init;
 function init() {
-    canvas = document.getElementById("my-canvas")
-    ctx = canvas.getContext('2d')
-    ctx.fillRect(0,0,350,250)
-    ctx.fillStyle= "blue"
+    canvas = document.getElementById("my-canvas");
+    ctx = canvas.getContext('2d');  
+    ctx.fillStyle= "aqua";
+    ctx.fillRect(150,100,350,250);    
+    ctx.strokeStyle="red";
+    ctx.strokeRect(150,100,350,250);
 }
