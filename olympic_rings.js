c47a1e3cb92a7549eb7ce18baa5f64fa07484036
Alex Yang <alexyang@Alexs-Air.hitronhub.home>
2018-02-02 18:14:36 -0800

    js

--- olympic_rings.js
+++ olympic_rings.js
@@ -4,3 +4,8 @@ window.onload = init;
 function init() {
     canvas = document.getElementById("my-canvas");
     ctx = canvas.getContext('2d');
+}
+
+function bluering() {
+    ctx.fillstyle = "blue"
+}
