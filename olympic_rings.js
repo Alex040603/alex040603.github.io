5c48750309d945d6090747d470d418ccc589d7c1
Alex Yang <alexyang@Alexs-Air.hitronhub.home>
2018-02-09 17:36:20 -0800

    js

--- olympic_rings.js
+++ olympic_rings.js
@@ -5,13 +5,16 @@ function init() {
     canvas = document.getElementById("my-canvas");
     ctx = canvas.getContext('2d');
     bluering();
+    blueringtop();
     yellowring();
+    blackring();
+    redwing();
 }
 
 function bluering() {
     ctx.beginPath();
     ctx.strokeStyle = "blue";
-    ctx.arc(100, 100, 70, 0, 1 * Math.PI);
+    ctx.arc(100, 100, 70, 0, Math.PI);
     ctx.lineWidth = 7;
     ctx.stroke();
 }
@@ -20,8 +23,31 @@ function bluering() {
 function yellowring() {
     ctx.beginPath();
     ctx.strokeStyle = "yellow";
-    ctx.arc(200, 100, 70, 0, 2 * Math.PI);
+    ctx.arc(190, 170, 70, 0, 2 * Math.PI);
     ctx.lineWidth = 7;
     ctx.stroke();
 }
 
+function blueringtop() {
+    ctx.beginPath();
+    ctx.strokeStyle = "blue";
+    ctx.arc(100, 100, 70, Math.PI, 2 * Math.PI);
+    ctx.lineWidth = 7;
+    ctx.stroke();
+}
+
+function blackring() {
+    ctx.beginPath();
+    ctx.strokeStyle = "black";
+    ctx.arc(280, 100, 70, 0, 2 * Math.PI);
+    ctx.lineWidth = 7;
+    ctx.stroke();
+}
+
+function redring() {
+    ctx.beginPath();
+    ctx.strokeStyle = "red";
+    ctx.arc(200, 100, 70, 0, 2 * Math.PI);
+    ctx.lineWidth = 7;
+    ctx.stroke();
+}
