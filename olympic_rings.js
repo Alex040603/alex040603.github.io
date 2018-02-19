9d7c9faa18a910c91e5f003752e247c3679f7248
Alex Yang <alexyang@Alexs-Air.hitronhub.home>
2018-02-19 10:59:15 -0800

    JS.C

--- olympic_rings.js
+++ olympic_rings.js
@@ -4,15 +4,25 @@ window.onload = init;
 function init() {
     canvas = document.getElementById("my-canvas");
     ctx = canvas.getContext('2d');
+    yellowringtop(); 
     blueringtop();
-    // yellowring();
-    blueringbottom();
+    blueringbottom(); 
+    yellowringbottom();
+    blackringbottom();
+   
 }
 
+function yellowringtop() {
+    ctx.beginPath();
+    ctx.strokeStyle = "yellow";
+    ctx.arc(160, 190, 70, 5 * Math.PI/4, Math.PI/4);
+    ctx.lineWidth = 7;
+    ctx.stroke();
+}
 function blueringtop() {
     ctx.beginPath();
     ctx.strokeStyle = "blue";
-    ctx.arc(100, 100, 70, 5 * Math.PI/4, Math.PI/4);
+    ctx.arc(90, 90, 70, 5 * Math.PI/4, Math.PI/4);
     ctx.lineWidth = 7;
     ctx.stroke();
 }
@@ -20,11 +30,26 @@ function blueringtop() {
 function blueringbottom() {
     ctx.beginPath();
     ctx.strokeStyle = "blue";
-    ctx.arc(100, 100, 70, Math.PI/4, 5 * Math.PI/4);
+    ctx.arc(90, 90, 70, Math.PI/4, 5 * Math.PI/4);
     ctx.lineWidth = 7;
     ctx.stroke();
 }
 
+function yellowringbottom() {
+    ctx.beginPath();
+    ctx.strokeStyle = "yellow";
+    ctx.arc(160, 190, 70, Math.PI/4, 5 * Math.PI/4);
+    ctx.lineWidth = 7;
+    ctx.stroke();
+} 
+
+function blackringbottom() {
+    ctx.beginPath();
+    ctx.strokeStyle = "black";
+    ctx.arc(250, 105, 70, Math.PI/4, 5 * Math.PI/4);
+    ctx.lineWidth = 7;
+    ctx.stroke();
+}
 
 
 
