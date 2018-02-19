6d4f3e775a6c4d6e226d722bad3ca4361b7c6aff
Alex Yang <alexyang@Alexs-Air.hitronhub.home>
2018-02-19 11:56:11 -0800

    JS.C

--- olympic_rings.js
+++ olympic_rings.js
@@ -4,22 +4,35 @@ window.onload = init;
 function init() {
     canvas = document.getElementById("my-canvas");
     ctx = canvas.getContext('2d');
-    yellowringtop(); 
-    blueringtop();
-    blueringbottom(); 
-    yellowringbottom();
-    blackringbottom();
+    blueyellowringleft(); 
+    blueyellowringtworight();
+    //blueringright();
+    //yellowringbottom();
+    //blackringleft();
    
 }
 
-function yellowringtop() {
+function blueyellowringleft() {
+    ctx.beginPath();
+    ctx.strokeStyle = "blue";
+    ctx.arc(90, 90, 70, Math.PI/4, 5 * Math.PI/4);
+    ctx.lineWidth = 7;
+    ctx.stroke(); 
+    //yellowring left part
     ctx.beginPath();
     ctx.strokeStyle = "yellow";
-    ctx.arc(160, 190, 70, 5 * Math.PI/4, Math.PI/4);
+    ctx.arc(160, 190, 70, Math.PI/4, 5 * Math.PI/4);
     ctx.lineWidth = 7;
     ctx.stroke();
 }
-function blueringtop() {
+
+function blueyellowringtworight() {
+    ctx.beginPath();
+    ctx.strokeStyle = "yellow";
+    ctx.arc(160, 190, 70, 5 * Math.PI/4,3*Math.PI/2);
+    ctx.lineWidth = 7;
+    ctx.stroke(); 
+    //bluering right
     ctx.beginPath();
     ctx.strokeStyle = "blue";
     ctx.arc(90, 90, 70, 5 * Math.PI/4, Math.PI/4);
@@ -27,29 +40,19 @@ function blueringtop() {
     ctx.stroke();
 }
 
-function blueringbottom() {
-    ctx.beginPath();
-    ctx.strokeStyle = "blue";
-    ctx.arc(90, 90, 70, Math.PI/4, 5 * Math.PI/4);
-    ctx.lineWidth = 7;
-    ctx.stroke();
+function blueringright() {
+    
 }
 
-function yellowringbottom() {
-    ctx.beginPath();
-    ctx.strokeStyle = "yellow";
-    ctx.arc(160, 190, 70, Math.PI/4, 5 * Math.PI/4);
-    ctx.lineWidth = 7;
-    ctx.stroke();
-} 
 
-function blackringbottom() {
-    ctx.beginPath();
-    ctx.strokeStyle = "black";
-    ctx.arc(250, 105, 70, Math.PI/4, 5 * Math.PI/4);
-    ctx.lineWidth = 7;
-    ctx.stroke();
-}
+
+// function blackringleft() {
+//     ctx.beginPath();
+//     ctx.strokeStyle = "black";
+//     ctx.arc(240, 100, 70, Math.PI/4, 5 * Math.PI/4);
+//     ctx.lineWidth = 7;
+//     ctx.stroke();
+// }
 
 
 
