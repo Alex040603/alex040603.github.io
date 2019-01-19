f57e0c30b2ec2bbcb38b4ec40390515f4d176cd9
Alex Yang <alexyang@Alexs-McBook.hitronhub.home>
2019-01-18 17:51:16 -0800

    Pacman Project progress

--- animation.js
+++ animation.js
@@ -1,14 +1,15 @@
 var canvas;
 var ctx;
-const UNIT = 5;
+const UNIT = 30;
 window.onload = init;
 
 function init (){
-    canvas = document.getElementById ("animation-canvas");
-    ctx = canvas.getcontext ("2d");
+    canvas = document.getElementById("animation-canvas");
+    ctx = canvas.getContext("2d");
+    
+    //drawPacman();
+    //drawPacman2();
+    drawPacman3();
+    drawRuler(1);
 }
 
-function drawcharacter() {
-    ctx.fillRect (0,0,0,0);
-    ctx.fillStyle("yellow");
-}
---
+++ drawing/pacman.js
@@ -0,0 +1,54 @@
+function drawPacman1() {
+    ctx.fillStyle = "rgb(251,255,0)";
+    //the top half (until the inner most part of the mouth)
+    ctx.fillRect(4 * UNIT, 0 * UNIT, 5 * UNIT, 1 * UNIT);
+    ctx.fillRect(2 * UNIT, 1 * UNIT, 9 * UNIT, 1 * UNIT);  
+    ctx.fillRect(1 * UNIT, 2 * UNIT, 11 * UNIT, 1 * UNIT);
+    ctx.fillRect(1 * UNIT, 3 * UNIT, 11 * UNIT, 1 * UNIT);
+    ctx.fillRect(0 * UNIT, 4 * UNIT, 10 * UNIT, 1 * UNIT);
+    ctx.fillRect(0 * UNIT, 5 * UNIT, 7 * UNIT, 1 * UNIT);
+    ctx.fillRect(0 * UNIT, 6 * UNIT, 4 * UNIT, 1 * UNIT);
+    //bottom half
+    ctx.fillRect(0 * UNIT, 7 * UNIT, 7 * UNIT, 1 * UNIT);
+    ctx.fillRect(0 * UNIT, 8 * UNIT, 10 * UNIT, 1 * UNIT);    
+    ctx.fillRect(1 * UNIT, 9 * UNIT, 11 * UNIT, 1 * UNIT);
+    ctx.fillRect(1 * UNIT, 10 * UNIT, 11 * UNIT, 1 * UNIT);
+    ctx.fillRect(2* UNIT, 11 * UNIT, 9 * UNIT, 1 * UNIT);
+    ctx.fillRect(4 * UNIT, 12 * UNIT, 5 * UNIT, 1 * UNIT);
+}
+// Pacman Chewing (2)
+function drawPacman2() {
+    ctx.fillStyle = "rgb(251,255,0)";
+    ctx.fillRect(4 * UNIT, 0 * UNIT, 5 * UNIT, 1 * UNIT);
+    ctx.fillRect(2 * UNIT, 1 * UNIT, 7 * UNIT, 1 * UNIT);  
+    ctx.fillRect(1 * UNIT, 2 * UNIT, 7 * UNIT, 1 * UNIT);
+    ctx.fillRect(1 * UNIT, 3 * UNIT, 6 * UNIT, 1 * UNIT);
+    ctx.fillRect(0 * UNIT, 4 * UNIT, 6 * UNIT, 1 * UNIT);
+    ctx.fillRect(0 * UNIT, 5 * UNIT, 5 * UNIT, 1 * UNIT);
+    ctx.fillRect(0 * UNIT, 6 * UNIT, 4 * UNIT, 1 * UNIT);
+    //bottom half
+    ctx.fillRect(0 * UNIT, 7 * UNIT, 5 * UNIT, 1 * UNIT);
+    ctx.fillRect(0 * UNIT, 8 * UNIT, 6 * UNIT, 1 * UNIT);    
+    ctx.fillRect(1 * UNIT, 9 * UNIT, 6 * UNIT, 1 * UNIT);
+    ctx.fillRect(1 * UNIT, 10 * UNIT, 7 * UNIT, 1 * UNIT);
+    ctx.fillRect(2 * UNIT, 11 * UNIT, 7 * UNIT, 1 * UNIT);
+    ctx.fillRect(4 * UNIT, 12 * UNIT, 5 * UNIT, 1 * UNIT); 
+}
+// Pacman Ball (3)
+function drawPacman3(){
+    ctx.fillStyle = "rgb(251,255,0)";
+    ctx.fillRect(4 * UNIT, 0 * UNIT, 5 * UNIT, 1 * UNIT);
+    ctx.fillRect(2 * UNIT, 1 * UNIT, 9 * UNIT, 1 * UNIT);  
+    ctx.fillRect(1 * UNIT, 2 * UNIT, 11 * UNIT, 1 * UNIT);
+    ctx.fillRect(1 * UNIT, 3 * UNIT, 11 * UNIT, 1 * UNIT);
+    ctx.fillRect(0 * UNIT, 4 * UNIT, 10 * UNIT, 1 * UNIT);
+    ctx.fillRect(0 * UNIT, 5 * UNIT, 7 * UNIT, 1 * UNIT);
+    ctx.fillRect(0 * UNIT, 6 * UNIT, 4 * UNIT, 1 * UNIT);
+    //bottom half
+    ctx.fillRect(0 * UNIT, 7 * UNIT, 7 * UNIT, 1 * UNIT);
+    ctx.fillRect(0 * UNIT, 8 * UNIT, 10 * UNIT, 1 * UNIT);    
+    ctx.fillRect(1 * UNIT, 9 * UNIT, 11 * UNIT, 1 * UNIT);
+    ctx.fillRect(1 * UNIT, 10 * UNIT, 11 * UNIT, 1 * UNIT);
+    ctx.fillRect(2* UNIT, 11 * UNIT, 9 * UNIT, 1 * UNIT);
+    ctx.fillRect(4 * UNIT, 12 * UNIT, 5 * UNIT, 1 * UNIT);
+}
