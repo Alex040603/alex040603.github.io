2273b143eaca02e9efc2c8795da82ad43e0c8bb5
Alex Yang <alexyang@Alexs-McBook.hitronhub.home>
2019-01-26 19:33:26 -0800

    started animation process

--- drawing/pacman.js
+++ drawing/pacman.js
@@ -41,12 +41,12 @@ function drawPacman3(){
     ctx.fillRect(2 * UNIT, 1 * UNIT, 9 * UNIT, 1 * UNIT);  
     ctx.fillRect(1 * UNIT, 2 * UNIT, 11 * UNIT, 1 * UNIT);
     ctx.fillRect(1 * UNIT, 3 * UNIT, 11 * UNIT, 1 * UNIT);
-    ctx.fillRect(0 * UNIT, 4 * UNIT, 10 * UNIT, 1 * UNIT);
-    ctx.fillRect(0 * UNIT, 5 * UNIT, 7 * UNIT, 1 * UNIT);
-    ctx.fillRect(0 * UNIT, 6 * UNIT, 4 * UNIT, 1 * UNIT);
+    ctx.fillRect(0 * UNIT, 4 * UNIT, 13 * UNIT, 1 * UNIT);
+    ctx.fillRect(0 * UNIT, 5 * UNIT, 13 * UNIT, 1 * UNIT);
+    ctx.fillRect(0 * UNIT, 6 * UNIT, 13 * UNIT, 1 * UNIT);
     //bottom half
-    ctx.fillRect(0 * UNIT, 7 * UNIT, 7 * UNIT, 1 * UNIT);
-    ctx.fillRect(0 * UNIT, 8 * UNIT, 10 * UNIT, 1 * UNIT);    
+    ctx.fillRect(0 * UNIT, 7 * UNIT, 13 * UNIT, 1 * UNIT);
+    ctx.fillRect(0 * UNIT, 8 * UNIT, 13 * UNIT, 1 * UNIT);    
     ctx.fillRect(1 * UNIT, 9 * UNIT, 11 * UNIT, 1 * UNIT);
     ctx.fillRect(1 * UNIT, 10 * UNIT, 11 * UNIT, 1 * UNIT);
     ctx.fillRect(2* UNIT, 11 * UNIT, 9 * UNIT, 1 * UNIT);
