a6ff43a6c1452fcdf3a5f412be385a172f18ce27
Alex Yang <alexyang@Alexs-McBook.hitronhub.home>
2019-02-22 16:26:44 -0800

    working on ghost

--- drawing/ghost.js
+++ drawing/ghost.js
@@ -6,9 +6,14 @@ function drawGhostOrange() {
     ctx.fillRect(3 * UNIT, 3 * UNIT, 10 * UNIT, 1 * UNIT)
     ctx.fillRect(2 * UNIT, 4 * UNIT, 12 * UNIT, 3 * UNIT)
     ctx.fillStyle = "rgb(255,255,55)";//yellow
-    ctx.fillRect(5 * UNIT, 4 * UNIT, 2 * UNIT, 1 * UNIT)
-    ctx.fillRect(11 * UNIT, 4 * UNIT, 2 * UNIT, 1 * UNIT )
+    ctx.fillRect(5 * UNIT, 4 * UNIT, 2 * UNIT, 2 * UNIT)
+    ctx.fillRect(11 * UNIT, 4 * UNIT, 2 * UNIT, 1 * UNIT)
     ctx.fillRect(4 * UNIT, 5 * UNIT, 4 * UNIT, 1 * UNIT)
     ctx.fillRect(10 * UNIT, 5 * UNIT, 4 * UNIT, 1 * UNIT)
-    //ctx.fillRect()
+    ctx.fillRect(10 * UNIT, 6 * UNIT, 4 * UNIT, 1 * UNIT)
+    ctx.fillRect(4 * UNIT, 6 * UNIT, 4 * UNIT, 1 * UNIT)
+    ctx.fillRect(100 * UNIT, 8 * UNIT, 2 * UNIT, 1 * UNIT)
+    ctx.fillStyle = "rgb(244,156,63)";//the bottom part after the pupil (orange)
+    ctx.fillRect(1 * UNIT, 7 * UNIT, 14 * UNIT, 5 * UNIT)
+    
 }
