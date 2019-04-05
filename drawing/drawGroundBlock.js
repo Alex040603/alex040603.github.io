b03704922ec4f6069929a53c53406ec3aaff04c8
Alex Yang <alexyang@Alexs-McBook.hitronhub.home>
2019-04-05 16:38:01 -0700

    finished grass block

--- drawing/drawGroundBlock.js
+++ drawing/drawGroundBlock.js
@@ -1,3 +1,14 @@
+function drawGroundBlocks(x,y,numBLock){
+    const GROUND_WIDTH = 16 * UNIT;
+    for(i=0; i<numBLock; i++){
+        ctx.save();
+        ctx.translate(x * UNIT, y * UNIT);
+        ctx.translate(i * GROUND_WIDTH, 0);
+        drawGroundBlock();
+        ctx.restore();
+
+    }
+}
 function drawGroundBlock(){
     ctx.fillStyle = "#77AD4C";
     //the top grass part
@@ -26,13 +37,87 @@ function drawGroundBlock(){
     ctx.fillRect(10 * UNIT, 1 * UNIT, 1 * UNIT, 15 * UNIT);
     ctx.fillRect(11 * UNIT, 3 * UNIT, 4 * UNIT, 13 * UNIT);
     ctx.fillRect(15 * UNIT, 4 * UNIT, 1 * UNIT, 12 * UNIT);
-    //extra colord pixels on the dirt
+    ctx.fillRect(7 * UNIT, 6 * UNIT, 1 * UNIT, 1 * UNIT);    
+    //the blue pixels
+    ctx.fillStyle = "#859A9F"
+    ctx.fillRect(2 * UNIT, 10 * UNIT, 1 * UNIT, 1 * UNIT);
+    ctx.fillRect(9 * UNIT, 7 * UNIT, 1 * UNIT, 1 * UNIT);
+    ctx.fillRect(10 * UNIT, 15 * UNIT, 1 * UNIT, 1 * UNIT);
+    ctx.fillRect(12 * UNIT, 11 * UNIT, 1 * UNIT, 1 * UNIT);
+    //extra colord pixels on the dirt (tan color i think)
     ctx.fillStyle = "#CD9B6F"
     ctx.fillRect(2 * UNIT, 4 * UNIT, 1 * UNIT, 1 * UNIT);
     ctx.fillRect(3 * UNIT, 5 * UNIT, 1 * UNIT, 1 * UNIT);
     ctx.fillRect(4 * UNIT, 6 * UNIT, 1 * UNIT, 1 * UNIT);
     ctx.fillRect(5 * UNIT, 5 * UNIT, 1 * UNIT, 3 * UNIT);
     ctx.fillRect(6 * UNIT, 8 * UNIT, 1 * UNIT, 1 * UNIT);
-    //Also, on the dirt block, the blacked out spots are the ones completed above. Start wherever.
+    ctx.fillRect(5 * UNIT, 12 * UNIT, 3 * UNIT, 1 * UNIT);
+    ctx.fillRect(7 * UNIT, 10 * UNIT, 1 * UNIT, 2 * UNIT);
+    ctx.fillRect(8 * UNIT, 8 * UNIT, 3 * UNIT, 1 * UNIT);
+    ctx.fillRect(0 * UNIT, 7 * UNIT, 1 * UNIT, 2 * UNIT);
+    ctx.fillRect(0 * UNIT, 11 * UNIT, 1 * UNIT, 2 * UNIT);
+    ctx.fillRect(0 * UNIT, 14 * UNIT, 1 * UNIT, 1 * UNIT);
+    ctx.fillRect(2 * UNIT, 8 * UNIT, 1 * UNIT, 1 * UNIT);
+    ctx.fillRect(3 * UNIT, 10 * UNIT, 1 * UNIT, 4 * UNIT);
+    ctx.fillRect(5 * UNIT, 14 * UNIT, 1 * UNIT, 1 * UNIT);
+    ctx.fillRect(8 * UNIT, 13 * UNIT, 1 * UNIT, 2 * UNIT);
+    ctx.fillRect(8 * UNIT, 5 * UNIT, 1 * UNIT, 1 * UNIT);
+    ctx.fillRect(10 * UNIT, 5 * UNIT, 2 * UNIT, 1 * UNIT);
+    ctx.fillRect(11 * UNIT, 4 * UNIT, 1 * UNIT, 1 * UNIT);
+    ctx.fillRect(10 * UNIT, 14 * UNIT, 1 * UNIT, 1 * UNIT);
+    ctx.fillRect(9 * UNIT, 11 * UNIT, 5 * UNIT, 1 * UNIT);
+    ctx.fillRect(10 * UNIT, 12 * UNIT, 2 * UNIT, 1 * UNIT);
+    ctx.fillRect(13 * UNIT, 12 * UNIT, 3 * UNIT, 1 * UNIT);
+    ctx.fillRect(14 * UNIT, 9 * UNIT, 2 * UNIT, 1 * UNIT);
+    ctx.fillRect(13 * UNIT, 8 * UNIT, 1 * UNIT, 1 * UNIT);    
+    ctx.fillRect(12 * UNIT, 6 * UNIT, 1 * UNIT, 2 * UNIT); 
+    ctx.fillRect(13 * UNIT, 5 * UNIT, 1 * UNIT, 2 * UNIT); 
+    ctx.fillRect(14 * UNIT, 4 * UNIT, 1 * UNIT, 2 * UNIT); 
+    ctx.fillRect(15 * UNIT, 6 * UNIT, 1 * UNIT, 1 * UNIT); 
+    ctx.fillRect(13 * UNIT, 15 * UNIT, 1 * UNIT, 1 * UNIT);
+
+    //lighter brown pixels
+    ctx.fillStyle = "#9D7556"
+    ctx.fillRect(5 * UNIT, 4 * UNIT, 2 * UNIT, 1 * UNIT);
+    ctx.fillRect(8 * UNIT, 4 * UNIT, 3 * UNIT, 1 * UNIT);
+    ctx.fillRect(10 * UNIT, 3 * UNIT, 1 * UNIT, 1 * UNIT);
+    ctx.fillRect(13 * UNIT, 4 * UNIT, 1 * UNIT, 1 * UNIT);
+    ctx.fillRect(12 * UNIT, 5 * UNIT, 1 * UNIT, 1 * UNIT);
+    ctx.fillRect(10 * UNIT, 7 * UNIT, 2 * UNIT, 1 * UNIT);
+    ctx.fillRect(9 * UNIT, 6 * UNIT, 1 * UNIT, 1 * UNIT);
+    ctx.fillRect(7 * UNIT, 5 * UNIT, 1 * UNIT, 3 * UNIT);
+    ctx.fillRect(13 * UNIT, 7 * UNIT, 3 * UNIT, 1 * UNIT);
+    ctx.fillRect(14 * UNIT, 8 * UNIT, 1 * UNIT, 1 * UNIT);
+    ctx.fillRect(14 * UNIT, 10 * UNIT, 1 * UNIT, 1 * UNIT);
+    ctx.fillRect(11 * UNIT, 9 * UNIT, 1 * UNIT, 1 * UNIT);
+    ctx.fillRect(9 * UNIT, 10 * UNIT, 4 * UNIT, 1 * UNIT);
+    ctx.fillRect(8 * UNIT, 9 * UNIT, 2 * UNIT, 1 * UNIT);
+    ctx.fillRect(15 * UNIT, 11 * UNIT, 1 * UNIT, 1 * UNIT);
+    ctx.fillRect(11 * UNIT, 9 * UNIT, 1 * UNIT, 1 * UNIT)
+    ctx.fillRect(10 * UNIT, 10 * UNIT, 1 * UNIT, 1 * UNIT);
+    ctx.fillRect(0 * UNIT, 6 * UNIT, 2 * UNIT, 1 * UNIT);
+    ctx.fillRect(1 * UNIT, 5 * UNIT, 2 * UNIT, 1 * UNIT);
+    ctx.fillRect(2 * UNIT, 7 * UNIT, 1 * UNIT, 1 * UNIT);
+    ctx.fillRect(3 * UNIT, 6 * UNIT, 1 * UNIT, 1 * UNIT);
+    ctx.fillRect(6 * UNIT, 5 * UNIT, 1 * UNIT, 1 * UNIT);  
+    ctx.fillRect(8 * UNIT, 6 * UNIT, 1 * UNIT, 1 * UNIT);  
+    ctx.fillRect(14 * UNIT, 13 * UNIT, 1 * UNIT, 1 * UNIT);
+    ctx.fillRect(0 * UNIT, 13 * UNIT, 2 * UNIT, 1 * UNIT);
+    ctx.fillRect(1 * UNIT, 12 * UNIT, 1 * UNIT, 1 * UNIT);
+    ctx.fillRect(3 * UNIT, 14 * UNIT, 1 * UNIT, 2 * UNIT);
+    ctx.fillRect(4 * UNIT, 14 * UNIT, 1 * UNIT, 1 * UNIT);
+    ctx.fillRect(5 * UNIT, 13 * UNIT, 1 * UNIT, 1 * UNIT);
+    ctx.fillRect(5 * UNIT, 9 * UNIT, 1 * UNIT, 1 * UNIT);
+    ctx.fillRect(3 * UNIT, 7 * UNIT, 1 * UNIT, 1 * UNIT);
+    ctx.fillRect(4 * UNIT, 8 * UNIT, 1 * UNIT, 1 * UNIT);
+    ctx.fillRect(12 * UNIT, 13 * UNIT,1  * UNIT, 2 * UNIT);
+    ctx.fillRect(5 * UNIT, 11 * UNIT, 1 * UNIT, 1 * UNIT);
+    ctx.fillRect(6 * UNIT, 15 * UNIT, 2 * UNIT, 1 * UNIT);
+    ctx.fillRect(7 * UNIT, 14 * UNIT, 1 * UNIT, 1 * UNIT);
+    ctx.fillRect(15 * UNIT, 14 * UNIT,1  * UNIT, 1 * UNIT);
+    
+    //Partially the blacked out spots (Black spot sizs dont matter) are the ones you have to 
+    //complete with the same color above. Start wherever.
+    //April 4th - Done for now ignore blacked out pixels and change based on disgresion
 
 }
