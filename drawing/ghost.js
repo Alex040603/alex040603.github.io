f910ae137dc75f08260a16860128d73e51280dbc
Alex Yang <alexyang@Alexs-McBook.hitronhub.home>
2019-05-03 17:40:01 -0700

    worked on animation for characters (pacman & ghost)

--- drawing/ghost.js
+++ drawing/ghost.js
@@ -1,20 +1,35 @@
-function drawGhost(x,y,color) {
+function drawGhost(x,y,color,frame) {
+    var ghostColor;
+    var frames
     const RED_COLOR="#FF0000";
-    const LAVANDER_COLOR="#FFB8FF"
-    const AQUA_COLOR="#00FFFF"
-    const ORANGE_COLOR="#FFB852"
+    const LAVANDER_COLOR="#FFB8FF";
+    const AQUA_COLOR="#00FFFF";
+    const ORANGE_COLOR="#FFB852";
     if (color=="red") {
-
+        ghostColor = RED_COLOR;
+    }else if(color=="lavander"){
+        ghostColor = LAVANDER_COLOR;
+    }else if (color=="aqua"){
+        ghostColor = AQUA_COLOR;
+    }else if (color=="orange"){
+        ghostColor = ORANGE_COLOR;
     }
+    //tentacle if
+    if (frame=="1") {
+        frames = "1"
+    } else if (frame=="2") {
+        frames = "2"
+    }
+
     ctx.save();
     ctx.translate(x * UNIT,y * UNIT);
-    ctx.fillStyle = "#FFB852";//(orange)
+    ctx.fillStyle = ghostColor;
     //the top to the pixel before the blue pupil 
     ctx.fillRect(5 * UNIT, 0 * UNIT, 4 * UNIT, 1 * UNIT);
     ctx.fillRect(3 * UNIT, 1 * UNIT, 8 * UNIT, 1 * UNIT);
     ctx.fillRect(2 * UNIT, 2 * UNIT, 10 * UNIT, 1 * UNIT);
     ctx.fillRect(1 * UNIT, 3 * UNIT, 12 * UNIT, 3 * UNIT);
-    ctx.fillStyle = "#FFB852";//the bottom part after the pupil until the tentacles (orange)
+    ctx.fillStyle = ghostColor;
     ctx.fillRect(0 * UNIT, 6 * UNIT, 14 * UNIT, 6 * UNIT);
     ctx.fillStyle = "#FDFEFF"; //the white eyes
     ctx.fillRect(4 * UNIT, 3 * UNIT, 2 * UNIT, 2 * UNIT);
@@ -26,12 +41,13 @@ function drawGhost(x,y,color) {
     ctx.fillRect(4 * UNIT, 6 * UNIT, 2 * UNIT, 2 * UNIT);
     ctx.fillRect(9 * UNIT, 6 * UNIT, 4 * UNIT, 1 * UNIT);
     ctx.fillRect(10 * UNIT, 7 * UNIT, 2 * UNIT, 1 * UNIT);
-    tentacles();
-    pupils();
+    tentacles(ghostColor);
+    //tentacles_frame_two();
+    drawPupils();
     ctx.restore();
 }
-function tentacles(){
-    ctx.fillStyle = "#FFB852";//(oragne)
+function tentacles(color){
+    ctx.fillStyle = color;
     ctx.fillRect(0 * UNIT, 12 * UNIT, 1 * UNIT, 2 * UNIT);
     ctx.fillRect(0 * UNIT, 12 * UNIT, 2 * UNIT, 1 * UNIT);
     ctx.fillRect(3 * UNIT, 12 * UNIT, 3 * UNIT, 1 * UNIT);
@@ -41,13 +57,19 @@ function tentacles(){
     ctx.fillRect(12 * UNIT, 12 * UNIT, 2 * UNIT, 1 * UNIT);
     ctx.fillRect(13 * UNIT, 13 * UNIT, 1 * UNIT, 1 * UNIT);
 }
-// function tentacles_frame_two(){
-//     ctx.fillStyle = "#FFB852";//(oragne)
-//     ctx.fillRect(13 * UNIT, )
-// }
-function pupils(){
-    ctx.fillStyle = "#3B60AD";//blue
+function tentacles_frame_two(){
+    ctx.fillStyle = ghostColor;
+    ctx.fillRect(1 * UNIT, 12 * UNIT, 2 * UNIT, 2 * UNIT);
+    ctx.fillRect(3 * UNIT, 12 * UNIT, 1 * UNIT, 1 * UNIT);
+    ctx.fillRect(5 * UNIT, 12 * UNIT, 1 * UNIT, 1 * UNIT);
+    ctx.fillRect(6 * UNIT, 12 * UNIT, 2 * UNIT, 2 * UNIT);
+    ctx.fillRect(8 * UNIT, 12 * UNIT, 1 * UNIT, 1 * UNIT);
+    ctx.fillRect(10 * UNIT, 12 * UNIT, 1 * UNIT, 1 * UNIT);
+    ctx.fillRect(11 * UNIT, 12 * UNIT, 2 * UNIT, 2 * UNIT);
+
+}
+function drawPupils(){
+    ctx.fillStyle = "#2373BD";
     ctx.fillRect(5 * UNIT, 5 * UNIT, 2 * UNIT, 2 * UNIT);
     ctx.fillRect(11 * UNIT, 5 * UNIT, 2 * UNIT, 2 * UNIT);
 }
-//
--- drawing/pacman.js
+++ drawing/pacman.js
@@ -1,11 +1,11 @@
-function drawPacman1(x,y) {
+function drawPacman1(x, y) {
     ctx.save();
     ctx.translate(x * UNIT, y * UNIT);
 
     ctx.fillStyle = "rgb(251,255,0)";
     //the top half (until the inner most part of the mouth)
     ctx.fillRect(4 * UNIT, 0 * UNIT, 5 * UNIT, 1 * UNIT);
-    ctx.fillRect(2 * UNIT, 1 * UNIT, 9 * UNIT, 1 * UNIT);  
+    ctx.fillRect(2 * UNIT, 1 * UNIT, 9 * UNIT, 1 * UNIT);
     ctx.fillRect(1 * UNIT, 2 * UNIT, 11 * UNIT, 1 * UNIT);
     ctx.fillRect(1 * UNIT, 3 * UNIT, 11 * UNIT, 1 * UNIT);
     ctx.fillRect(0 * UNIT, 4 * UNIT, 10 * UNIT, 1 * UNIT);
@@ -13,21 +13,21 @@ function drawPacman1(x,y) {
     ctx.fillRect(0 * UNIT, 6 * UNIT, 4 * UNIT, 1 * UNIT);
     //bottom half
     ctx.fillRect(0 * UNIT, 7 * UNIT, 7 * UNIT, 1 * UNIT);
-    ctx.fillRect(0 * UNIT, 8 * UNIT, 10 * UNIT, 1 * UNIT);    
+    ctx.fillRect(0 * UNIT, 8 * UNIT, 10 * UNIT, 1 * UNIT);
     ctx.fillRect(1 * UNIT, 9 * UNIT, 11 * UNIT, 1 * UNIT);
     ctx.fillRect(1 * UNIT, 10 * UNIT, 11 * UNIT, 1 * UNIT);
-    ctx.fillRect(2* UNIT, 11 * UNIT, 9 * UNIT, 1 * UNIT);
+    ctx.fillRect(2 * UNIT, 11 * UNIT, 9 * UNIT, 1 * UNIT);
     ctx.fillRect(4 * UNIT, 12 * UNIT, 5 * UNIT, 1 * UNIT);
     ctx.restore();
 }
 // Pacman Chewing (2)
-function drawPacman2() {
+function drawPacman2(x, y) {
     ctx.save();
     ctx.translate(x * UNIT, y * UNIT);
-    
+
     ctx.fillStyle = "rgb(251,255,0)";
     ctx.fillRect(4 * UNIT, 0 * UNIT, 5 * UNIT, 1 * UNIT);
-    ctx.fillRect(2 * UNIT, 1 * UNIT, 7 * UNIT, 1 * UNIT);  
+    ctx.fillRect(2 * UNIT, 1 * UNIT, 7 * UNIT, 1 * UNIT);
     ctx.fillRect(1 * UNIT, 2 * UNIT, 7 * UNIT, 1 * UNIT);
     ctx.fillRect(1 * UNIT, 3 * UNIT, 6 * UNIT, 1 * UNIT);
     ctx.fillRect(0 * UNIT, 4 * UNIT, 6 * UNIT, 1 * UNIT);
@@ -35,21 +35,21 @@ function drawPacman2() {
     ctx.fillRect(0 * UNIT, 6 * UNIT, 4 * UNIT, 1 * UNIT);
     //bottom half
     ctx.fillRect(0 * UNIT, 7 * UNIT, 5 * UNIT, 1 * UNIT);
-    ctx.fillRect(0 * UNIT, 8 * UNIT, 6 * UNIT, 1 * UNIT);    
+    ctx.fillRect(0 * UNIT, 8 * UNIT, 6 * UNIT, 1 * UNIT);
     ctx.fillRect(1 * UNIT, 9 * UNIT, 6 * UNIT, 1 * UNIT);
     ctx.fillRect(1 * UNIT, 10 * UNIT, 7 * UNIT, 1 * UNIT);
     ctx.fillRect(2 * UNIT, 11 * UNIT, 7 * UNIT, 1 * UNIT);
-    ctx.fillRect(4 * UNIT, 12 * UNIT, 5 * UNIT, 1 * UNIT); 
+    ctx.fillRect(4 * UNIT, 12 * UNIT, 5 * UNIT, 1 * UNIT);
     ctx.restore();
 }
 // Pacman Ball (3)
-function drawPacman3(){
+function drawPacman3(x, y) {
     ctx.save();
     ctx.translate(x * UNIT, y * UNIT);
 
     ctx.fillStyle = "rgb(251,255,0)";
     ctx.fillRect(4 * UNIT, 0 * UNIT, 5 * UNIT, 1 * UNIT);
-    ctx.fillRect(2 * UNIT, 1 * UNIT, 9 * UNIT, 1 * UNIT);  
+    ctx.fillRect(2 * UNIT, 1 * UNIT, 9 * UNIT, 1 * UNIT);
     ctx.fillRect(1 * UNIT, 2 * UNIT, 11 * UNIT, 1 * UNIT);
     ctx.fillRect(1 * UNIT, 3 * UNIT, 11 * UNIT, 1 * UNIT);
     ctx.fillRect(0 * UNIT, 4 * UNIT, 13 * UNIT, 1 * UNIT);
@@ -57,10 +57,10 @@ function drawPacman3(){
     ctx.fillRect(0 * UNIT, 6 * UNIT, 13 * UNIT, 1 * UNIT);
     //bottom half
     ctx.fillRect(0 * UNIT, 7 * UNIT, 13 * UNIT, 1 * UNIT);
-    ctx.fillRect(0 * UNIT, 8 * UNIT, 13 * UNIT, 1 * UNIT);    
+    ctx.fillRect(0 * UNIT, 8 * UNIT, 13 * UNIT, 1 * UNIT);
     ctx.fillRect(1 * UNIT, 9 * UNIT, 11 * UNIT, 1 * UNIT);
     ctx.fillRect(1 * UNIT, 10 * UNIT, 11 * UNIT, 1 * UNIT);
-    ctx.fillRect(2* UNIT, 11 * UNIT, 9 * UNIT, 1 * UNIT);
+    ctx.fillRect(2 * UNIT, 11 * UNIT, 9 * UNIT, 1 * UNIT);
     ctx.fillRect(4 * UNIT, 12 * UNIT, 5 * UNIT, 1 * UNIT);
     ctx.restore();
 }
