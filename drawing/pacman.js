6e6d9a5c59c6dd701ec2ad4558215735faa0b850
Alex Yang <alexyang@Alexs-McBook.hitronhub.home>
2019-06-07 17:43:02 -0700

    worked on pacman death frames

--- drawing/pacman.js
+++ drawing/pacman.js
@@ -1,3 +1,142 @@
+function drawPacmanDie(x, y, frame) {
+    //on the png, the death frames with 2 black dots on it are the ones that are done
+    //one dot means they are in progress
+    ctx.save();
+    ctx.translate(x*UNIT,y*UNIT);
+    if (frame == 1) {
+        ctx.translate(1*UNIT,1*UNIT);
+        ctx.fillStyle = "rgb(251,255,0)";
+        ctx.fillRect(4 * UNIT, 0 * UNIT, 5 * UNIT, 1 * UNIT);
+        ctx.fillRect(2 * UNIT, 1 * UNIT, 9 * UNIT, 1 * UNIT);
+        ctx.fillRect(1 * UNIT, 2 * UNIT, 11 * UNIT, 1 * UNIT);
+        ctx.fillRect(1 * UNIT, 3 * UNIT, 11 * UNIT, 1 * UNIT);
+        ctx.fillRect(0 * UNIT, 4 * UNIT, 13 * UNIT, 1 * UNIT);
+        ctx.fillRect(0 * UNIT, 5 * UNIT, 13 * UNIT, 1 * UNIT);
+        ctx.fillRect(0 * UNIT, 6 * UNIT, 13 * UNIT, 1 * UNIT);
+        //bottom half
+        ctx.fillRect(0 * UNIT, 7 * UNIT, 13 * UNIT, 1 * UNIT);
+        ctx.fillRect(0 * UNIT, 8 * UNIT, 13 * UNIT, 1 * UNIT);
+        ctx.fillRect(1 * UNIT, 9 * UNIT, 11 * UNIT, 1 * UNIT);
+        ctx.fillRect(1 * UNIT, 10 * UNIT, 11 * UNIT, 1 * UNIT);
+        ctx.fillRect(2 * UNIT, 11 * UNIT, 9 * UNIT, 1 * UNIT);
+        ctx.fillRect(4 * UNIT, 12 * UNIT, 5 * UNIT, 1 * UNIT);
+    } else if (frame == 2) {
+        //looks like clam
+        ctx.translate(1*UNIT,1*UNIT);
+        ctx.fillStyle = "rgb(251,255,0)";
+        ctx.fillRect(0 * UNIT, 2 * UNIT, 2 * UNIT, 1 * UNIT);
+        ctx.fillRect(11 * UNIT, 2 * UNIT, 2 * UNIT, 1 * UNIT);
+        ctx.fillRect(0 * UNIT, 3 * UNIT, 3 * UNIT, 1 * UNIT);
+        ctx.fillRect(10 * UNIT, 3 * UNIT, 3 * UNIT, 1 * UNIT);
+        ctx.fillRect(0 * UNIT, 4 * UNIT, 4 * UNIT, 1 * UNIT);
+        ctx.fillRect(9 * UNIT, 4 * UNIT, 4 * UNIT, 1 * UNIT);
+        ctx.fillRect(0 * UNIT, 5 * UNIT, 5 * UNIT, 1 * UNIT);
+        ctx.fillRect(8 * UNIT, 5 * UNIT, 5 * UNIT, 1 * UNIT);
+        ctx.fillRect(0 * UNIT, 6 * UNIT, 6 * UNIT, 1 * UNIT);
+        ctx.fillRect(7 * UNIT, 6 * UNIT, 6 * UNIT, 1 * UNIT);
+        ctx.fillRect(1 * UNIT, 7 * UNIT, 11 * UNIT, 2 * UNIT);
+        ctx.fillRect(2 * UNIT, 9 * UNIT, 9 * UNIT, 1 * UNIT);
+        ctx.fillRect(4 * UNIT, 10 * UNIT, 5 * UNIT, 1 * UNIT);
+    } else if (frame == 3) {
+        //ulgier clam
+        ctx.translate(0*UNIT,1*UNIT);
+        ctx.fillStyle = "rgb(251,255,0)";
+        ctx.fillRect(1 * UNIT, 3 * UNIT, 1 * UNIT, 1 * UNIT);
+        ctx.fillRect(13 * UNIT, 3 * UNIT, 1 * UNIT, 1 * UNIT);
+        ctx.fillRect(0 * UNIT, 4 * UNIT, 4 * UNIT, 1 * UNIT);
+        ctx.fillRect(11 * UNIT, 4 * UNIT, 4 * UNIT, 1 * UNIT);
+        ctx.fillRect(0 * UNIT, 5 * UNIT, 5 * UNIT, 1 * UNIT);
+        ctx.fillRect(10 * UNIT, 5 * UNIT, 5 * UNIT, 1 * UNIT);
+        ctx.fillRect(0 * UNIT, 6 * UNIT, 7 * UNIT, 1 * UNIT);
+        ctx.fillRect(8 * UNIT, 6 * UNIT, 7 * UNIT, 1 * UNIT);
+        ctx.fillRect(1 * UNIT, 7 * UNIT, 13 * UNIT, 2 * UNIT);
+        ctx.fillRect(2 * UNIT, 9 * UNIT, 11 * UNIT, 1 * UNIT);
+        ctx.fillRect(4 * UNIT, 7 * UNIT, 3 * UNIT, 1 * UNIT);
+        ctx.fillRect(4 * UNIT, 10 * UNIT, 3 * UNIT, 1 * UNIT);
+        ctx.fillRect(8 * UNIT, 10 * UNIT, 3 * UNIT, 1 * UNIT);
+    } else if (frame == 4) {
+        ctx.translate(0*UNIT,2*UNIT);
+        ctx.fillStyle = "rgb(251,255,0)";
+        ctx.fillRect(0 * UNIT, 5 * UNIT, 3 * UNIT, 1 * UNIT);
+        ctx.fillRect(12 * UNIT, 5 * UNIT, 3 * UNIT, 1 * UNIT);
+        ctx.fillRect(0 * UNIT, 6 * UNIT, 6 * UNIT, 1 * UNIT);
+        ctx.fillRect(9 * UNIT, 6 * UNIT, 6 * UNIT, 1 * UNIT);
+        ctx.fillRect(0 * UNIT, 7 * UNIT, 15 * UNIT, 1 * UNIT);
+        ctx.fillRect(1 * UNIT, 8 * UNIT, 13 * UNIT, 1 * UNIT);
+        ctx.fillRect(2 * UNIT, 9 * UNIT, 11 * UNIT, 1 * UNIT);
+        ctx.fillRect(4 * UNIT, 10 * UNIT, 3 * UNIT, 1 * UNIT);
+        ctx.fillRect(8 * UNIT, 10 * UNIT, 3 * UNIT, 1 * UNIT);
+    } else if (frame == 5) {
+        ctx.translate(0*UNIT,2*UNIT);
+        ctx.fillStyle = "rgb(251,255,0)";
+        ctx.fillRect(0 * UNIT, 6 * UNIT, 4 * UNIT, 1 * UNIT);
+        ctx.fillRect(11 * UNIT, 6 * UNIT, 4 * UNIT, 1 * UNIT);
+        ctx.fillRect(0 * UNIT, 7 * UNIT, 15 * UNIT, 2 * UNIT);
+        ctx.fillRect(1 * UNIT, 9 * UNIT, 13 * UNIT, 1 * UNIT);
+        ctx.fillRect(3 * UNIT, 10 * UNIT, 4 * UNIT, 1 * UNIT);
+        ctx.fillRect(8 * UNIT, 10 * UNIT, 4 * UNIT, 1 * UNIT);
+    } else if (frame == 6) {
+        ctx.translate(0*UNIT,0*UNIT);
+        ctx.fillStyle = "rgb(251,255,0)";
+        ctx.fillRect(4 * UNIT, 8 * UNIT, 7 * UNIT, 1 * UNIT);
+        ctx.fillRect(0 * UNIT, 9 * UNIT, 15 * UNIT, 2 * UNIT);
+        ctx.fillRect(1 * UNIT, 11 * UNIT, 13 * UNIT, 1 * UNIT);
+        ctx.fillRect(3 * UNIT, 12 * UNIT, 4 * UNIT, 1 * UNIT);
+        ctx.fillRect(8 * UNIT, 12 * UNIT, 4 * UNIT, 1 * UNIT);
+    } else if (frame == 7) {
+        ctx.translate(0*UNIT,0*UNIT);
+        ctx.fillStyle = "rgb(251,255,0)";
+        ctx.fillRect(6 * UNIT, 8 * UNIT, 3 * UNIT, 1 * UNIT);
+        ctx.fillRect(4 * UNIT, 9 * UNIT, 7 * UNIT, 1 * UNIT);
+        ctx.fillRect(1 * UNIT, 10 * UNIT, 13 * UNIT, 1 * UNIT);
+        ctx.fillRect(0 * UNIT, 11 * UNIT, 15 * UNIT, 1 * UNIT);
+        ctx.fillRect(1 * UNIT, 12 * UNIT, 6 * UNIT, 1 * UNIT);
+        ctx.fillRect(8 * UNIT, 12 * UNIT, 6 * UNIT, 1 * UNIT);
+        ctx.fillRect(3 * UNIT, 13 * UNIT, 3 * UNIT, 1 * UNIT);
+        ctx.fillRect(9 * UNIT, 13 * UNIT, 3 * UNIT, 1 * UNIT);
+    } else if (frame == 8) {
+        ctx.translate(0*UNIT,0*UNIT);
+        ctx.fillStyle = "rgb(251,255,0)";
+        ctx.fillRect(7 * UNIT, 8 * UNIT, 1 * UNIT, 1 * UNIT);
+        ctx.fillRect(6 * UNIT, 9 * UNIT, 3 * UNIT, 1 * UNIT);
+        ctx.fillRect(4 * UNIT, 10 * UNIT, 7 * UNIT, 1 * UNIT);
+        ctx.fillRect(3 * UNIT, 11 * UNIT, 9 * UNIT, 1 * UNIT);
+        ctx.fillRect(6 * UNIT, 9 * UNIT, 3 * UNIT, 1 * UNIT);
+        ctx.fillRect(1 * UNIT, 12 * UNIT, 13 * UNIT, 1 * UNIT);
+        ctx.fillRect(1 * UNIT, 13 * UNIT, 6 * UNIT, 1 * UNIT);
+        ctx.fillRect(8 * UNIT, 13 * UNIT, 6 * UNIT, 1 * UNIT);
+        ctx.fillRect(2 * UNIT, 14 * UNIT, 4 * UNIT, 1 * UNIT);
+        ctx.fillRect(9 * UNIT, 14 * UNIT, 4 * UNIT, 1 * UNIT);
+    }else if (frame==9) {
+        ctx.translate(0*UNIT,0*UNIT);
+        ctx.fillStyle = "rgb(251,255,0)";
+        ctx.fillRect(7 * UNIT, 8 * UNIT, 1 * UNIT, 1 * UNIT);
+        ctx.fillRect(6 * UNIT, 9 * UNIT, 3 * UNIT, 1 * UNIT);
+        ctx.fillRect(5 * UNIT, 10 * UNIT, 5 * UNIT, 2 * UNIT);
+        ctx.fillRect(4 * UNIT, 12 * UNIT, 7 * UNIT, 1 * UNIT);
+        ctx.fillRect(3 * UNIT, 13 * UNIT, 9 * UNIT, 1 * UNIT);
+        ctx.fillRect(4 * UNIT, 14 * UNIT, 3 * UNIT, 1 * UNIT);
+        ctx.fillRect(8 * UNIT, 14 * UNIT, 3 * UNIT, 1 * UNIT);
+    }else if (frame==10) {
+        ctx.translate(0*UNIT,0*UNIT);
+        ctx.fillStyle = "rgb(251,255,0)";
+        ctx.fillRect(7 * UNIT, 8 * UNIT, 1 * UNIT, 2 * UNIT);
+        ctx.fillRect(7 * UNIT, 8 * UNIT, 1 * UNIT, 1 * UNIT);
+        ctx.fillRect(6 * UNIT, 9 * UNIT, 3 * UNIT, 3 * UNIT);
+        ctx.fillRect(5 * UNIT, 12 * UNIT, 5 * UNIT, 1 * UNIT);
+        ctx.fillRect(6 * UNIT, 13 * UNIT, 1 * UNIT, 1 * UNIT);
+        ctx.fillRect(8 * UNIT, 13 * UNIT, 1 * UNIT, 1 * UNIT);
+    }else if (frame==11) {
+        ctx.translate(0*UNIT,0*UNIT);
+        ctx.fillStyle = "rgb(251,255,0)";
+        ctx.fillRect(7 * UNIT, 8 * UNIT, 1 * UNIT, 6 * UNIT);
+    }else if (frame==12) {
+        ctx.translate(0*UNIT,0*UNIT);
+        ctx.fillStyle = "rgb(251,255,0)";
+        ctx.fillRect(6 * UNIT, 6 * UNIT, 1 * UNIT, 1 * UNIT);
+    }
+    ctx.restore();
+}
 function drawPacman1(x, y) {
     ctx.save();
     ctx.translate(x * UNIT, y * UNIT);
